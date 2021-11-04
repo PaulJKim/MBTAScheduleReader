488bff094d6ace38cbce91977676918a672cd534
Paul Kim <pkim49@gatech.edu>
2021-11-04 14:50:30 -0400

    Add filtering based on direction

--- lib/mbta_schedule_reader/next_train_calculator.ex
+++ lib/mbta_schedule_reader/next_train_calculator.ex
@@ -9,9 +9,9 @@ defmodule MbtaScheduleReader.NextTrainCalculator do
     Enum.reject(data["data"], fn e -> e["attributes"]["departure_time"] == nil end)
   end
 
-  @spec filterForDirection(nil | maybe_improper_list | map) :: list
-  def filterForDirection(data) do
-    Enum.filter(data, fn e -> e["attributes"]["direction_id"] == 0 end)
+  @spec filterForDirection(nil | maybe_improper_list | map, charlist) :: list
+  def filterForDirection(data, direction) do
+    Enum.filter(data, fn e -> e["attributes"]["direction_id"] == elem(Integer.parse(direction), 0) end)
   end
 
   def iterateOverPredictions([head | tail]) do
--- lib/mbta_schedule_reader_web/controllers/metro_schedule_controller.ex
+++ lib/mbta_schedule_reader_web/controllers/metro_schedule_controller.ex
@@ -10,9 +10,16 @@ defmodule MbtaScheduleReaderWeb.MetroScheduleController do
   @spec getNextTime(Plug.Conn.t(), any) :: Plug.Conn.t()
   def getNextTime(conn, %{"line" => line, "stop" => stop, "direction" => direction} = params) do
     # Idea: Separate service for translating stop name (ex. Harvard Ave) to parent stop id (ex. place-harvd)
+    # Would probably require us to maintain a mapping of stop name to parent stop id.
+    # Alexa could maybe handle accounting for different variations of "Harvard Avenue", for example
 
-    # How to pass direction to filter for direction??
-    predictions = getPredictions(stop) |> filterForDepartureTimeExists() |> filterForDirection() |> calculateNextTrain()
+    # How to pass direction variable to filter for direction??
+    IO.inspect(params)
+    predictions =
+      getPredictions(stop) |>
+      filterForDepartureTimeExists() |>
+      filterForDirection(direction) |>
+      calculateNextTrain()
     json(conn, List.first(predictions))
   end
 end
